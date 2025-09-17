188f2a4a89a4c702dc0ed66e5b5a746ae89383a5
MliliGenes <saad.elm.77@gmail.com>
2025-09-17 22:33:32 +0100

    draaaakulaaaa

--- argo.c
+++ argo.c
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <unistd.h>
 
+
+typedef enum {
+	MAP,
+	INTEGER,
+	STRING
+} e_type;
+
+typedef struct map {
+	struct pair	*data;
+	size_t		size;
+} t_map;
+
 typedef struct	json {
-	enum {
-		MAP,
-		INTEGER,
-		STRING
-	} type;
+	e_type type;
 	union {
-		struct {
-			struct pair	*data;
-			size_t		size;
-		} map;
+		t_map map;
 		int	integer;
 		char	*string;
 	};
@@ -114,39 +120,97 @@ int	argo(json *dst, FILE *stream);
 
 int main ( int ac, char** av ) {
 	if ( ac != 2 ) return 1;
-	FILE *stream = fopen(av[1]);
+	FILE *stream = fopen(av[1], "r");
 	
 	json *head = NULL;
 }
 
-typedef enum { STRING, NUMBER, MAP, E } t_type;
-
-t_type get_type( char c ) 
+e_type get_type( char c ) 
 {
 	if (c == '\"') return STRING;
-	if (isdigit(c) || c == '-') return NUMBER;
+	if (isdigit(c) || c == '-') return INTEGER;
 	if (c == '{') return MAP;
-	return E;
+	return -1;
+}
+
+json *create_node( int type, void *data ) {
+	json *node = malloc( sizeof(json) );
+	if (!node) return NULL;
+	node->type = type;
+	switch ( type ) {
+		case STRING:
+			node->string = data;
+			break;
+		case INTEGER:
+			node->integer = *(int*)data;
+			break;
+		case MAP:
+			node->map = *(t_map *)data;
+			break;
+	}
+	return node;
 }
 
 json* extract_str ( FILE* s ) {
+	
+	if (!accept(s, '\"'))
+		return NULL;
 
+	int cap = 16;
+	int len = 0;
+	char *str = malloc(cap);
+	if (!str) return NULL;
 
+	while ( true ) {
 
+		if (len >= cap) {
+			cap *= 2;
+			char *new_str = realloc(str, cap);
+			if (!new_str) {
+				free(str);
+				return NULL;
+			}
+			str = new_str;
+		}
 
+		char le_peek = peek(s);
+		if (le_peek == '\"') {
+			getc(s);
+			break;
+		}
 
+		if (le_peek == EOF) {
+			free(str);
+			return NULL;
+		}
+
+		if (le_peek == '\\') {
+			getc(s);
+		}
+
+		str[len++] = getc(s);
+
+	}
+
+	return create_node( STRING, str );
 
 }
 
 json *parse ( json* head, FILE* stream ) {
 
 	char le_peek = peek( stream );
-	
-	t_type type = get_type ( le_peek );
-	
-	if ( type == STRING )
-		return extract_str();
 
+	e_type le_type = get_type ( le_peek );
+	if  ( le_type == -1 )
+		return NULL;
+
+	if ( le_type == STRING )
+		return extract_str( stream );
+	if ( le_type == INTEGER )
+
+	if ( le_type == MAP)
+
+	return NULL;
 }
 
 int argo(json* dst, FILE* stream) {
